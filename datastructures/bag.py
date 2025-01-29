42d415238df2a83700e45de677c86cc069906c72
kjbrown24 <kjbrown@willamette.edu>
2025-01-29 02:15:08 -0800

    Final

--- datastructures/bag.py
+++ datastructures/bag.py
@@ -1,32 +1,52 @@
 from typing import Iterable, Optional
 from datastructures.ibag import IBag, T
-#Test change
+
 
 class Bag(IBag[T]):
     def __init__(self, *items: Optional[Iterable[T]]) -> None:
         self.items_counts = {}
-        self.keys = self.items_counts.keys()
 
     def add(self, item: T) -> None:
         if item in self.items_counts:
-            if item in self.keys:
-                self.items_counts[item] += 1
+            self.items_counts[item] += 1
+        elif item == None:
+            raise TypeError("None is not an item")
         else:
             self.items_counts[item] = 1
 
     def remove(self, item: T) -> None:
-        raise NotImplementedError("remove method not implemented")
+        if item in self.items_counts:
+            count_before = self.items_counts[item]
+            count_after = count_before - 1
+            self.items_counts[item] = count_after
+        else:
+            raise ValueError("Item not in bag")
 
     def count(self, item: T) -> int:
-        return self.items_counts[item]
+        counter = 0
+        if item in self.items_counts:
+            counter += self.items_counts[item]
+            return counter
+        else:
+            return counter
+
     def __len__(self) -> int:
-        raise NotImplementedError("__len__ method not implemented")
+        length = 0
+        for item in self.items_counts:
+            length += self.items_counts[item]
+        return length
 
     def distinct_items(self) -> int:
-        raise NotImplementedError("distinct_items method not implemented")
+        distincts = set()
+        for item in self.items_counts:
+            distincts.add(item)
+        return distincts
 
     def __contains__(self, item) -> bool:
-        raise NotImplementedError("__contains__ method not implemented")
+        if item in self.items_counts:
+            return True
+        else:
+            return False
 
     def clear(self) -> None:
-        raise NotImplementedError("clear method not implemented")
+        self.items_counts.clear()
